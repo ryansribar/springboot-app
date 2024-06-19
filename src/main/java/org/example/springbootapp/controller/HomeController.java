a33255d49debf03d187792fe033a6a3acbaf1498
Ryan Sribar <ryansribar@gmail.com>
2024-06-19 16:30:53 -0400

    Finalized the header.

--- src/main/java/org/example/springbootapp/controller/HomeController.java
+++ src/main/java/org/example/springbootapp/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         // Add any attributes needed for the landing page here
-        model.addAttribute("title", "Welcome to My Personal Website");
+        model.addAttribute("title", "Home");
         model.addAttribute("description", "This is a brief introduction to my personal " +
                 "website where you can find more about me and my projects");
         return "index";
@@ -18,9 +18,9 @@ public class HomeController {
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "About Me");
-        model.addAttribute("description", "Hi, my name is Ryan Sribar. I am a third year " +
-                "Computer Science and Economics student at the University of Virginia.");
+        model.addAttribute("title", "About");
+        model.addAttribute("description", "Hello, my name is Ryan Sribar. I'm from Northern " +
+                "Virginia and currently attend the University of Virginia studying Computer Science and Economics.");
         return "about";
     }
 }
